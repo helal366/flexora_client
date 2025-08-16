73b9640dbfb68a10512f42c8a532d1d731ef342e
Helal366 <helal56876@gmail.com>
2025-08-16 17:22:54 +0600

    navbar is sticky

--- src/components/headerComponents/Navbar.jsx
+++ src/components/headerComponents/Navbar.jsx
@@ -5,7 +5,7 @@ import NavbarEnd from './NavbarEnd';
 
 const Navbar = () => {
     return (
-        <section className='shadow-[-4px_4px_10px_rgba(0,0,0,0.25)] border border-gray-500/50 rounded'>
+        <section className='shadow-[-4px_4px_10px_rgba(0,0,0,0.25)] border border-gray-500/50 rounded sticky top-0 z-20 bg-teal-50'>
             <div className="navbar bg-teal-50 shadow-sm padding">
                 <NavbarStart />
                 <NavbarCenter />
