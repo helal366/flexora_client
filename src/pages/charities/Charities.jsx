34506673d98c8db86ad3a540632b2800a106afc6
Helal366 <helal56876@gmail.com>
2025-08-17 11:58:26 +0600

    Charities component, fetched charities, loading and error added.

--- src/pages/charities/Charities.jsx
+++ src/pages/charities/Charities.jsx
@@ -1,10 +1,36 @@
+import { useQuery } from '@tanstack/react-query';
 import React from 'react';
+import useAxiosSecure from '../../hooks/useAxiosSecure';
+import Loading from '../../components/loadingComponents/Loading';
+import useAuth from './../../hooks/useAuth';
 
 const Charities = () => {
+    const {user}=useAuth();
+    const userEmail=user?.email
+    const axiosSecure=useAxiosSecure()
+    const {data: charities=[], isLoading, isError, error}=useQuery({
+        queryKey: ['charities'],
+        queryFn: async()=>{
+            const res=await axiosSecure.get(`/charities?email=${userEmail}`);
+            return res?.data
+        }
+    })
+    if(isLoading){
+        return <Loading/>
+    }
+    if(isError){
+        // error comes from Axios response or query failure
+    const backendMessage = error?.response?.data?.message || error.message;
     return (
-        <div>
-            All the Charities list here.
-        </div>
+      <div className="text-red-600 bg-red-100 border border-red-400 p-4 rounded">
+        {backendMessage}
+      </div>
+    );
+    }
+    return (
+        <section>
+            All the Charities list here: {charities.length}
+        </section>
     );
 };
 
