b57c0b9bfbecb22aa0af603eafdd715baf2ec1db
Helal366 <helal56876@gmail.com>
2025-08-20 07:27:11 +0600

    At home page, charity request section, some text colors are updated for dark mode.

--- src/pages/homePage/CardCharityRequest.jsx
+++ src/pages/homePage/CardCharityRequest.jsx
@@ -29,15 +29,15 @@ const CardCharityRequest = ({ request }) => {
                     alt={request?.charity_name} />
             </figure>
             <div className='card-body'>
-                <h2 className="card-title mb-6"> {request?.charity_name} </h2>
+                <h2 className="card-title mb-6 two"> {request?.charity_name} </h2>
                 
                     <img
                         src={request?.donation_image}
                         alt={request?.donation_title || "Donated food"}
                         className="w-14 h-14 object-cover border-2 border-gray-400 rounded-md"
                     />
-                <p> <span className='two'>Food Donation Title :</span> {request?.donation_title}</p>
-                <p> <span className='two'>Charity Representative :</span> {request?.charity_representative_name}</p>
+                <p> <span className='two'>Food Donation Title :</span> <span className='text-black'>{request?.donation_title}</span></p>
+                <p> <span className='two'>Charity Representative :</span> <span className='text-black'>{request?.charity_representative_name}</span></p>
             </div>
         </section>
     );
