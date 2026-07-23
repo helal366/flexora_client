5da1cc06463b2a4acc04b8280f6f6f41e2a278b4
Helal366 <helal56876@gmail.com>
2026-07-23 19:28:24 +0600

    typescript error handled at cloudinary image file.

--- src/hooks/useCloudinaryImageUpload.tsx
+++ src/hooks/useCloudinaryImageUpload.tsx
@@ -1,9 +1,10 @@
+/// <reference types="vite/client" />
 import React from 'react';
 import { useMutation } from '@tanstack/react-query';
 import axios from 'axios';
 
 // 1. Upload function: makes a POST request to Cloudinary
-const uploadCloudinaryImage = async (file) => {
+const uploadCloudinaryImage = async (file:File) => {
   const formData = new FormData();
   formData.append("file", file);
   formData.append("upload_preset", import.meta.env.VITE_upload_preset);
