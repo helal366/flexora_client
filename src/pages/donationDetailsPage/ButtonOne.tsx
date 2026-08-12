1ca938cb32b8f2783f8f0d78e5a7da5ba8ea02b4
Helal366 <helal56876@gmail.com>
2026-08-13 01:33:36 +0600

    fix: fix button one typescript issues.

--- src/pages/donationDetailsPage/ButtonOne.tsx
+++ src/pages/donationDetailsPage/ButtonOne.tsx
@@ -1,12 +1,31 @@
-const ButtonOne = ({children, bg, hoverBg, textColor, onClick, disabled, isFavorited}) => {
-    return (
-        <button 
-        onClick={onClick}
-        disabled={disabled}
-        className={`btn ${bg} ${hoverBg} ${textColor} mb-6 ${isFavorited?'text-gray-600/50 cursor-not-allowed':''}`}>
-            {children}
-        </button>
-    );
+import { ReactNode } from "react";
+interface ButtonOneProps {
+  children: ReactNode;
+  bg: string;
+  hoverBg: string;
+  textColor: string;
+  onClick: () => void;
+  disabled?: boolean;
+  isFavorited?: boolean;
+}
+const ButtonOne = ({
+  children,
+  bg,
+  hoverBg,
+  textColor,
+  onClick,
+  disabled,
+  isFavorited,
+}: ButtonOneProps) => {
+  return (
+    <button
+      onClick={onClick}
+      disabled={disabled}
+      className={`btn ${bg} ${hoverBg} ${textColor} mb-6 ${isFavorited ? "text-gray-600/50 cursor-not-allowed" : ""}`}
+    >
+      {children}
+    </button>
+  );
 };
 
 export default ButtonOne;
