4620de91eca8b74d627e777125fc0ebb4bcbc342
Helal366 <helal56876@gmail.com>
2026-08-22 03:35:20 +0600

    fix: update ts at single ui

--- src/pages/donationDetailsPage/SingleUI.tsx
+++ src/pages/donationDetailsPage/SingleUI.tsx
@@ -1,14 +1,17 @@
-import React from 'react';
+interface ISingleUIProps{
+    label: string;
+    value:string
+}
 
-const SingleUI = ({label, value}) => {
-    if(!value){
-        return null
-    }
-    return (
-        <p className='text-teal-800 text-[15px]'>
-           <span className='font-semibold italic'>{label} : </span> {value}
-        </p>
-    );
+const SingleUI = ({ label, value }: ISingleUIProps) => {
+  if (!value) {
+    return null;
+  }
+  return (
+    <p className="text-teal-800 text-[15px]">
+      <span className="font-semibold italic">{label} : </span> {value}
+    </p>
+  );
 };
 
 export default SingleUI;
